568462ef9f136a7991bde1c34a85bf085534e970
Victor <chrollolucilfer1402@gmail.com>
2023-06-05 17:06:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return <div className="app min-h-screen"></div>;
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header>
+        <div className="logo">
+            
+        </div>
+    </header>
+  )
+};
