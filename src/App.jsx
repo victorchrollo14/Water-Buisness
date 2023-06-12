1ea390b4a8f45befdb859eaa92e91f6cd334147b
Victor <chrollolucilfer1402@gmail.com>
2023-06-12 22:46:24 +0530

    Fix Issue #6

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Hero } from "./components/Hero";
 import { About } from "./components/About";
 import Products from "./components/Products";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Products />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+let linksClass = "text-sm ml-6 list-disc hover:text-red-400";
+
+export const Footer = () => {
+  return (
+    <footer className="footer bg-blue-700 min-h-60 p-2 text-white flex flex-col justify-around md:flex-row">
+      <div className="logo m-5">
+        <img src={logo} alt="Logo" className="w-28 h-auto" />
+        <p className="max-w-sm text-sm mt-3 font-mono">
+          we supply clean healthy drinking water Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Optio quaerat inventore similique illum{" "}
+        </p>
+      </div>
+
+      <ul className="site-map m-5">
+        <li>
+          <h1 className="font-mono font-bold">Site Map</h1>
+        </li>
+        <li className={linksClass}>
+          <a href="#Home">Home</a>
+        </li>
+        <li className={linksClass}>
+          <a href="#Product">Product</a>
+        </li>
+        <li className={linksClass}>
+          <a href="#About">About</a>
+        </li>
+        <li className={linksClass}>
+          <a href="#Contact">Contact</a>
+        </li>
+      </ul>
+      <ul className="ph-nums text-white font-sans m-5">
+        <li className="font-mono font-bold">PHONE</li>
+        <li className={linksClass}>
+          <a href="tel:9620300513">9620300513</a>
+        </li>
+        <li className={linksClass}>
+          <a href="tel:9902702486">9902702486</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
